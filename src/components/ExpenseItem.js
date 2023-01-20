bfdb1ae5b6a2151cf4d04d7adc609107989b4ea8
Mahdi Hasan <mhasan@westminster.gov.uk>
2023-01-20 10:03:16 +0000

    Added some files

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,16 @@
 import "./ExpenseItem.css";
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const year = props.date.getFullYear();
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
-      <div className="expense-item_description">
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item_price">£{props.amount}</div>
+        <div className="expense-item__price">£{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
